d042a1afae7e276e74e030e864af7f84aaf47666
LepharamRamchiary <lepharamchiary@gmail.com>
2024-12-17 10:04:47 +0530

    add some models

--- pythonDjango/polls/admin.py
+++ pythonDjango/polls/admin.py
@@ -1,5 +1,24 @@
 from django.contrib import admin
-from .models import TeaVarity
+from .models import TeaVarity, TeaReview, TeaCertificate, Store
 
 # Register your models here.
-admin.site.register(TeaVarity)
+class TeaReviewInline(admin.TabularInline):
+    model = TeaReview
+    extra = 2
+
+class TeaVarityAdmin(admin.ModelAdmin):
+    list_display = ("name", "type", "price", "date_added")
+    inlines = [TeaReviewInline]
+
+class StoreAdmin(admin.ModelAdmin):
+    list_display = ("name", "location")
+    filter_horizontal = ("tea_varieties",)
+
+class TeaCertificateAdmin(admin.ModelAdmin):
+    list_display = ("tea", "certificate_number")
+
+
+admin.site.register(TeaVarity, TeaVarityAdmin)
+# admin.site.register(TeaReview, TeaReviewInline)
+admin.site.register(TeaCertificate, TeaCertificateAdmin)
+admin.site.register(Store, StoreAdmin)
--- pythonDjango/polls/models.py
+++ pythonDjango/polls/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 # Create your models here.
 class TeaVarity(models.Model):
@@ -18,3 +20,38 @@ class TeaVarity(models.Model):
     def __str__(self):
         return self.name
     
+
+# One to Many 
+class TeaReview(models.Model):
+    tea = models.ForeignKey(TeaVarity, on_delete=models.CASCADE, related_name="reviews")
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    # rating = models.IntegerField()
+    rating = models.IntegerField(
+        validators=[MinValueValidator(1), MaxValueValidator(5)],
+        help_text="Rating should be between 1 (lowest) and 5 (highest)."
+    )
+    comment = models.TextField()
+    date_added = models.DateTimeField(default=timezone.now)
+    
+    def __str__(self):
+        return f"{self.user.username} review for {self.tea.name}"
+    
+# many to many 
+class Store(models.Model):
+    name = models.CharField(max_length=100)
+    location = models.CharField(max_length=100)
+    tea_varieties = models.ManyToManyField(TeaVarity, related_name="stores")
+    
+    def __str__(self):
+        return self.name
+    
+
+#one to one 
+class TeaCertificate(models.Model):
+    tea = models.OneToOneField(TeaVarity, on_delete=models.CASCADE, related_name="certificate")
+    certificate_number = models.CharField(max_length=100)
+    issued_date = models.DateField(default=timezone.now)
+    valid_date = models.DateField()
+    
+    def __str__(self):
+        return f'Certificate for {self.tea.name}'
